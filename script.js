c433c00eb10f83091fd23d11672e47cceb68a6c3
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-07-13 17:03:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function showTab(id) {
     tab.classList.remove('active');
   });
   document.getElementById(id).classList.add('active');
+
+  const search = document.getElementById('searchContainer');
+  if (id === 'upload' || id === 'settings') {
+    search.style.display = 'none';
+  } else {
+    search.style.display = 'flex';
+  }
 }
