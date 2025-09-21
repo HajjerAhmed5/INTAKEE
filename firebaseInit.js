1992784478ec67cf7bebba75991f4903133cc48e
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-09-21 04:04:01 -0400

    Update firebaseInit.js

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,3 +1,9 @@
+// firebaseInit.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getStorage }     from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA7QkHJhdGju0-EEAzDqpe64dqZsPou9bs",
   authDomain: "intakee-24fb7.firebaseapp.com",
@@ -5,5 +11,10 @@ const firebaseConfig = {
   storageBucket: "intakee-24fb7.appspot.com",
   messagingSenderId: "156485573091",
   appId: "1:156485573091:web:99e0a28f5a1425f40fe8d6",
-  measurementId: "G-PN4ZF9Y347"
+  measurementId: "G-PN4ZF9V347"
 };
+
+export const app     = initializeApp(firebaseConfig);
+export const auth    = getAuth(app);
+export const db      = getFirestore(app);
+export const storage = getStorage(app);
