e52849086877fe092965b519d9666b1ebd777a0a
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-09-21 03:57:49 -0400

    Update firebaseInit.js
    Update firebase config with correct keys

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,17 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyA7QkHJhdGju0-EEAzDqpe64dqZsPou9bs",
   authDomain: "intakee-24fb7.firebaseapp.com",
   projectId: "intakee-24fb7",
-  storageBucket: "intakee-24fb7.firebasestorage.app",
-  appId: "YOUR_APP_ID",
+  storageBucket: "intakee-24fb7.appspot.com",
+  messagingSenderId: "156485573091",
+  appId: "1:156485573091:web:99e0a28f5a1425f40fe8d6",
+  measurementId: "G-PN4ZF9Y347"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
