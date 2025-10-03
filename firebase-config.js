449c9756cb3d18d64f320346f0479a6bd0c5a396
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-10-03 02:21:51 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfdfTXfVD3a3rB1HvkAZyy0vU2oPqP-o",
+  authDomain: "intake-prod-a8c8c.firebaseapp.com",
+  projectId: "intake-prod-a8c8c",
+  storageBucket: "intake-prod-a8c8c.appspot.com",
+  messagingSenderId: "523426559560",
+  appId: "1:523426559560:web:23636302a7791fa0d5ca7d",
+  measurementId: "G-RC1ZKW161"
+};
