127b09e8e0ffb72e82b6b7f8f993df6983ab258c
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-10-03 03:35:04 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCfdFTXdIVD3a3r1BHVkAZyyOuV2oPqP-o",
   authDomain: "intake-prod-a8c8c.firebaseapp.com",
   projectId: "intake-prod-a8c8c",
-  storageBucket: "intake-prod-a8c8c.firebasestorage.app",
+  storageBucket: "intake-prod-a8c8c.appspot.com",   // ✅ fixed
   messagingSenderId: "523426559560",
   appId: "1:523426559560:web:23663302a779f1a0d5ca7d",
   measurementId: "G-RC1ZZKW161"
