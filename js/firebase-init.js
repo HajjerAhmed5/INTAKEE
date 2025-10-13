989770344d5e7888045668b2e600eabc2cb973fe
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-10-13 00:58:08 -0400

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,23 +1,23 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// js/firebase-init.js
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyD0_tL8PxUvGT7JqCBj3tuL7s3Kipl5E6g",
-    authDomain: "intakee-5785e.firebaseapp.com",
-    projectId: "intakee-5785e",
-    storageBucket: "intakee-5785e.firebasestorage.app",
-    messagingSenderId: "40666230072",
-    appId: "1:40666230072:web:49dd5e7db91c8a38b565cd",
-    measurementId: "G-3C2YDV6T0G"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth, setPersistence, browserLocalPersistence } 
+  from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } 
+  from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyD0_tL8PxUvGT7JqCBj3tuL7s3Kipl5E6g",
+  authDomain: "intakee-5785e.firebaseapp.com",
+  projectId: "intakee-5785e",
+  storageBucket: "intakee-5785e.firebasestorage.app",
+  messagingSenderId: "40666230072",
+  appId: "1:40666230072:web:49dd5e7db91c8a38b565cd",
+  measurementId: "G-3C2YDV6T0G"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+await setPersistence(auth, browserLocalPersistence);
