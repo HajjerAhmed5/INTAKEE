29cb0828899a174eaa1520283589b3c91e31003e
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-12-06 17:07:04 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,21 +1,28 @@
 // firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-    getAuth, 
-    setPersistence, 
-    browserLocalPersistence 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { 
-    getFirestore 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { 
-    getStorage 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+// -------------------------------
+// MATCH VERSION WITH SCRIPT.JS
+// Firebase SDK 10.13.2 (Required)
+// -------------------------------
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 import { firebaseConfig } from "./firebase-config.js";
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
