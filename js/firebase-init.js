b727387123ddc02021fa20118cde0a54daf25795
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-12-06 15:07:43 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,23 +1,24 @@
-// js/firebase-init.js
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+    getAuth, 
+    setPersistence, 
+    browserLocalPersistence 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+    getFirestore 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getStorage 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth, setPersistence, browserLocalPersistence } 
-  from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { getFirestore } 
-  from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { firebaseConfig } from "./firebase-config.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD0_tL8PxUvGT7JqCBj3tuL7s3Kipl5E6g",
-  authDomain: "intakee-5785e.firebaseapp.com",
-  projectId: "intakee-5785e",
-  storageBucket: "intakee-5785e.firebasestorage.app",
-  messagingSenderId: "40666230072",
-  appId: "1:40666230072:web:49dd5e7db91c8a38b565cd",
-  measurementId: "G-3C2YDV6T0G"
-};
-
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
+// Keep users logged in
 await setPersistence(auth, browserLocalPersistence);
