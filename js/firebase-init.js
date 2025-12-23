0c5cc52a5409de67d5f6ee064a3da9182841487c
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-12-23 18:20:33 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,5 +1,3 @@
-// /js/firebase-init.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
@@ -7,10 +5,8 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-
 
 import { firebaseConfig } from "./firebase-config.js";
 
-// 🔥 Initialize ONCE
 const app = initializeApp(firebaseConfig);
 
-// 🔥 Export singletons
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
