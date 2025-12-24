3f2569842b4c12b905a186360869ecb24d44ae41
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-12-23 19:21:20 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
-// js/firebase-config.js
+// /js/firebase-config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyD_tL8PxUvGTJqCBj3tuL7s3KipL5E6g",
+  apiKey: "AIzaSyD0_tL8PxUvGT7JqCBj3tuL7s3Kipl5E6g",
   authDomain: "intakee-5785e.firebaseapp.com",
   projectId: "intakee-5785e",
-  storageBucket: "intakee-5785e.appspot.com",
+  storageBucket: "intakee-5785e.firebasestorage.app",
   messagingSenderId: "40666230072",
   appId: "1:40666230072:web:3e3875043b11d795b565cd"
 };
