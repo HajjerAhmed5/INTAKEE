d111a2b098b2812dbfdab1493a54ca5ea83192d0
HajjerAhmed5 <hajjerahmed16@gmail.com>
2025-12-29 03:31:52 -0500

    Delete js/firebase-config.js

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// /js/firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyD0_tL8PxUvGT7JqCBj3tuL7s3Kipl5E6g",
-  authDomain: "intakee-5785e.firebaseapp.com",
-  projectId: "intakee-5785e",
-  storageBucket: "intakee-5785e.firebasestorage.app",
-  messagingSenderId: "40666230072",
-  appId: "1:40666230072:web:3e3875043b11d795b565cd"
-};
